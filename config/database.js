cc64df675b86e55559c5028e14bb221d6dcb411c
befrdu <befrdu.gebreamlack@gmail.com>
2025-04-06 17:14:00 -0500

    update database config

--- config/database.js
+++ config/database.js
@@ -1,22 +1,29 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-//   max: 10, // set connection limit to 10
-//   idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-//   connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
-// });
+let pool;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
-  ssl: {
-    rejectUnauthorized: false, // Required for Heroku Postgres to allow self-signed certificates
-  },
-});
+if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
+    ssl: {
+      rejectUnauthorized: false, // Required for Heroku Postgres to allow self-signed certificates
+    },
+    max: 5, // set connection limit to 20
+    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+    connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    max: 10, // set connection limit to 10
+    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+    connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+  });
+}
 
 module.exports = pool;
