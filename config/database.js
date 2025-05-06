cd93c49328daf8a3ae4f5c0a513b7247dcd88d5c
befrdu <befrdu.gebreamlack@gmail.com>
2025-05-06 18:43:35 -0500

    use local database for testing

--- config/database.js
+++ config/database.js
@@ -1,30 +1,39 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-let pool;
+let pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'AuditApp@5233',
+  port: 5432,
+  max: 10, // set connection limit to 10
+  idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+  connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+});
 
-if (process.env.NODE_ENV === 'production') {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
-    ssl: {
-      require: true, // This will ensure that the SSL connection is required
-      rejectUnauthorized: false, // Required for Heroku Postgres to allow self-signed certificates
-    },
-    max: 5, // set connection limit to 20
-    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-    connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
-  });
-} else {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    max: 10, // set connection limit to 10
-    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-    connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
+//     ssl: {
+//       require: true, // This will ensure that the SSL connection is required
+//       rejectUnauthorized: false, // Required for Heroku Postgres to allow self-signed certificates
+//     },
+//     max: 5, // set connection limit to 20
+//     idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+//     connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+//   });
+// } else {
+//   pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//     max: 10, // set connection limit to 10
+//     idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+//     connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+//   });
+// }
 
 module.exports = pool;
