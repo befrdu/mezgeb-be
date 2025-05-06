1266b909d4feed23da3728ac3d03822cd0961785
befrdu <befrdu.gebreamlack@gmail.com>
2025-05-05 19:36:43 -0500

    revert config change

--- config/database.js
+++ config/database.js
@@ -5,17 +5,10 @@ let pool;
 
 if (process.env.NODE_ENV === 'production') {
   pool = new Pool({
-    //connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
+    connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
     ssl: {
       rejectUnauthorized: false, // Required for Heroku Postgres to allow self-signed certificates
     },
-    //added
-    user: process.env.D2B_USER,
-    host: process.env.D2B_HOST,
-    database: process.env.D2B_NAME,
-    password: process.env.D2B_PASSWORD,
-    port: process.env.D2B_PORT,
-    //added
     max: 5, // set connection limit to 20
     idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
     connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
