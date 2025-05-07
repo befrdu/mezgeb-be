94b136777eb18b0b51e2c585c0d04f7b59244f86
befrdu <befrdu.gebreamlack@gmail.com>
2025-05-06 19:52:12 -0500

    fixed error log

--- config/database.js
+++ config/database.js
@@ -4,17 +4,18 @@ require('dotenv').config();
 let pool;
 
 if (process.env.NODE_ENV === 'production') {
-  consolle.log('Connecting to production database in Heroku...');
+  console.log('Connecting to production database in Heroku...');
   console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // Use the database URL from environment variables
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-      require: true, // This will ensure that the SSL connection is required
-      rejectUnauthorized: false, // Required for Heroku Postgres to allow self-signed certificates
+      require: true,
+      rejectUnauthorized: false,
     },
-    max: 5, // set connection limit to 20
-    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-    connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
   });
 } else {
   pool = new Pool({
@@ -22,10 +23,10 @@ if (process.env.NODE_ENV === 'production') {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    max: 10, // set connection limit to 10
-    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-    connectionTimeoutMillis: 2000, // How long to wait for a connection to be established
+    port: Number(process.env.DB_PORT),
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
   });
 }
 
